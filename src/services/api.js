b526deee3841d11a71c8a6796589ebe83c7c740a
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-09-18 14:25:19 -0300

    Add getProductsFromCategoryAndQuery function

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,10 @@
 export async function getCategories() {
-  const fetchResult = await (await fetch('https://api.mercadolibre.com/sites/MLB/categories')).json();
-  return fetchResult;
+  const categories = await (await fetch('https://api.mercadolibre.com/sites/MLB/categories')).json();
+  return categories;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const products = await (await fetch(url)).json();
+  return products;
 }
