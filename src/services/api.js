279222b2d5e65adc3d0b35fd4812ab307a01835a
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-09-20 14:18:15 -0300

    Refactor

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 export async function getCategories() {
-  const categories = await (await fetch('https://api.mercadolibre.com/sites/MLB/categories')).json();
-  return categories;
+  return (await fetch('https://api.mercadolibre.com/sites/MLB/categories')).json();
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
