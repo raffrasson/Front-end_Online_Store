0c3d88b6ccd26001f5663516960012f21326f03b
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-09-20 17:32:34 -0300

    Add componetn cardbutton

---
+++ src/components/AddCartButton.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddCartButton extends React.Component {
+  constructor() {
+    super();
+    this.addItem = this.addItem.bind(this);
+  }
+
+  addItem() {
+    const { product } = this.props;
+    const productList = JSON.parse(localStorage.cartItems);
+    productList.push(product);
+    localStorage.cartItems = JSON.stringify(productList);
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="button"
+          value="Adicionar item no carrinho"
+          data-testid="product-detail-add-to-cart"
+          onClick={ this.addItem }
+        />
+      </div>
+    );
+  }
+}
+
+AddCartButton.propTypes = {
+  product: PropTypes.objectOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+  })).isRequired,
+};
+
+export default AddCartButton;
