fd354d0021d9bcd32d2afe15919167a5a67f6b94
juli0c3sar <jcmuchiutti@gmail.com>
2021-09-20 17:47:10 -0300

    requisito 05 completo

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import CartButton from './CartButton';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
       categories: [],
+      products: [],
       isLoading: true,
+      isLoadingProducts: false,
+      inputSearch: '',
     };
   }
 
@@ -20,26 +23,71 @@ class Search extends React.Component {
     this.setState({ categories, isLoading: false });
   }
 
+  getProduct = async () => {
+    await this.setState({ isLoadingProducts: true });
+    const { inputSearch } = this.state;
+    const products = await getProductsFromCategoryAndQuery('', inputSearch);
+    await this.setState({ products: products.results, isLoadingProducts: false });
+  }
+
+  handleChange = (event) => {
+    event.preventDefault();
+    this.setState({ inputSearch: event.target.value });
+  }
+
   renderCategories() {
     const { categories } = this.state;
     return (
       <div>
-        { categories.map((cat) => (
-          <p key={ cat.id } data-testid="category">{cat.name}</p>
+        {categories.map((cat) => (
+          <p key={ cat.id } data-testid="category">{ cat.name }</p>
+        ))}
+      </div>
+    );
+  }
+
+  renderProducts = () => {
+    const { products } = this.state;
+    return (
+      <div>
+        {products.map(({ id, title, thumbnail, price }) => (
+          <div key={ id } data-testid="product">
+            <img src={ thumbnail } alt="imagem do produto" />
+            <p>{ title }</p>
+            <p>{`R$:${price}`}</p>
+          </div>
         ))}
       </div>
     );
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, inputSearch, isLoadingProducts } = this.state;
     return (
       <div>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <form>
+          <input
+            type="text"
+            placeholder="Pesquisa Aqui"
+            name="barraDePesquisa"
+            value={ inputSearch }
+            onChange={ this.handleChange }
+            data-testid="query-input"
+          />
+          <button
+            type="button"
+            data-testid="query-button"
+            onClick={ this.getProduct }
+          >
+            Pesquisar
+          </button>
+        </form>
         <CartButton />
         {isLoading ? null : this.renderCategories()}
+        {isLoadingProducts ? null : this.renderProducts()}
       </div>
     );
   }
